625c87f2e164810a42286bf61bf0ed26b8cc67c9
davidtoral <davidtoralmoreno98@gmail.com>
2018-07-03 17:32:22 +0200

    Añadir slider y tratamientos

---
+++ public/js/slick/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   $('.slider').slick({
+   		//prevArrow:"<img class='a-left control-c prev slick-prev' src='./images/index/flechaizq.png'>",
+      	//nextArrow:"<img class='a-right control-c next slick-next' src='./images/index/flechader.png'>",
+		  autoplay: true,
+		  dots: true,
+		  arrows: false,
+		});
+    });
